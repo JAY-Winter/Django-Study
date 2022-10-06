18290c892ab742aa3373501cacfc68a9dc6affb1
JAY-Winter <rubat0@naver.com>
2022-10-06 16:37:58 +0900

    feat: forum model 구현

--- CRUD/forums/models.py
+++ CRUD/forums/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms import DateTimeField
 
 # Create your models here.
+class Forum(models.Model):
+    title = models.CharField(max_length=15)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
