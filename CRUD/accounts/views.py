b7b1fd0dd8884e45b938d80834865accd408cc82
JAY-Winter <rubat0@naver.com>
2022-10-07 01:20:56 +0900

    feat: 회원가입 기능 구현

--- CRUD/accounts/views.py
+++ CRUD/accounts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.db import reset_queries
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods, require_POST
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -8,8 +11,19 @@ def login():
 def logout():
     pass
 
-def signup():
-    pass
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
 
 def delete():
     pass
