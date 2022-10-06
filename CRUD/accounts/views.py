11036152c55738423ef20e0a8ce7499e89107c45
JAY-Winter <rubat0@naver.com>
2022-10-07 01:28:36 +0900

    feat: 로그인 기능 구현

--- CRUD/accounts/views.py
+++ CRUD/accounts/views.py
@@ -1,12 +1,24 @@
 from django.db import reset_queries
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.http import require_http_methods, require_POST
 from .forms import CustomUserCreationForm
 
 # Create your views here.
-
-def login():
-    pass
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('forums:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
 
 def logout():
     pass
