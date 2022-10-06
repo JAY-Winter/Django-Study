3b289597c5f4aec14ba3c5d635abc25935173c54
JAY-Winter <rubat0@naver.com>
2022-10-06 15:46:24 +0900

    feat: skelton views 구현

--- CRUD/accounts/views.py
+++ CRUD/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login():
+    pass
+
+def logout():
+    pass
+
+def signup():
+    pass
+
+def delete():
+    pass
+
+def update():
+    pass
+
+def change_password():
+    pass
--- CRUD/forums/views.py
+++ CRUD/forums/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index():
+    pass
+
+def create():
+    pass
+
+def update():
+    pass
+
+def delete():
+    pass
